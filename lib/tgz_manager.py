88f1831c9c6b73cbbe3afae8648ec5d7e628a93c
jose <jose@jose-K53SJ.(none)>
2013-05-07 23:43:55 -0600

    Fixing coding guidelines, comments and tests for tgz manager

--- lib/tgz_manager.py
+++ lib/tgz_manager.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
-__author__ = "Jose Antonio Navas"
-__copyright__ = "Copyright 2011, The QIIME Project"
-__credits__ = ["Jose Antonio Navas"]
+__author__ = "Jose Antonio Navas Molina"
+__copyright__ = "Copyright 2013, The Galaxy-QIIME Project"
+__credits__ = ["Jose Antonio Navas Molina"]
 __license__ = "GPL"
-__version__ = "1.4.0-dev"
-__maintainer__ = "Jose Antonio Navas"
+__version__ = "0.0.1-dev"
+__maintainer__ = "Jose Antonio Navas Molina"
 __email__ = "josenavasmolina@gmail.com"
 __status__ = "Development"
 
@@ -13,41 +13,45 @@ import tarfile
 from shutil import move
 from os import path
 
-ERROR_MSG = "'tgz_file' is not a gzip file!"
+ERROR_MSG = "%s is not a gzip file!"
 
-def compress_to_tgz(in_path, tgz_name):
-	"""
-		in_path: path to compress
-		tgz_name: name of the .tgz file
+def compress_to_tgz(in_path, tgz_fp):
+    """Generate a tgz file with the contents of the provided path
 
-		generates a .tgz file which contains the 'in_path' tree with name tgz_name
-	"""
-	t = tarfile.open(name = tgz_name, mode = 'w:gz')
-	t.add(in_path, path.basename(in_path))
-	t.close()
+    Input:
+        in_path: path to include in the tgz file
+        tgz_fp: path to the result tgz file
+    """
+    t = tarfile.open(name = tgz_fp, mode = 'w:gz')
+    t.add(in_path, path.basename(in_path))
+    t.close()
 
 def extract_from_tgz(tgz_file, output_path):
-	"""
-		tgz_file: filepath to .tgz file
-		output_path: path name of the output
+    """Extract the contents of the tgz file in the provided output path
 
-		extract the contents of tgz_file in a directory named 'output_path'.
-		If the tgz_file only contains one file, then is extracted in the
-		current directory an renamed as 'output_path'
+    Input:
+        tgz_file: filepath to the tgz file
+        output_path: path to the output directory
 
-		Raises a ValueError if tgz_file is not a tgz_file
-	"""
-	try:
-		t = tarfile.open(name = tgz_file, mode = 'r:gz')
-		names_list = t.getnames()
-		#if the tgz_file only has one file is not necessary to generate an output directory
-		if len(names_list) == 1:
-			t.extractall()
-			move(names_list[0], output_path)
-		else:
-			t.extractall(path=output_path)
-		t.close()
-	except tarfile.ReadError, e:
-			if str(e) == "not a gzip file":
-				raise ValueError, ERROR_MSG
-			raise tarfile.ReadError, e
+    If the tgz_file only contains one file, it is extracted and renamed as the
+    path indicated 'output_path'
+
+    Note: raises a ValueError if tgz_file is not a tgz_file
+        If there is any other error during the extraction, it propagates the
+        error raised by tarfile
+    """
+    try:
+        t = tarfile.open(name = tgz_file, mode = 'r:gz')
+        names_list = t.getnames()
+        # if the tgz_file only has one file is not necessary to generate an
+        # output directory
+        if len(names_list) == 1:
+            t.extractall()
+            move(names_list[0], output_path)
+        else:
+            t.extractall(path=output_path)
+        t.close()
+    except tarfile.ReadError, e:
+            if str(e) == "not a gzip file":
+                raise ValueError, ERROR_MSG % tgz_file
+            raise tarfile.ReadError, e
